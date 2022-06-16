d20ff69bfe83cca6fa5a2ca8807a0b06c8930b48
Ruslan Marchuk <mckruslan@gmail.com>
2022-06-16 12:48:45 +0200

    implement @FindBy

--- src/main/java/TestPage.java
+++ src/main/java/TestPage.java
@@ -1,28 +1,28 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class TestPage {
 
     private String baseUrl = "http://www.leafground.com/pages/checkbox.html";
-    private By languageCheckboxesListLocator = By.xpath(
-            "//label[text()='Select the languages that you know?']/following-sibling::input");
 
-    private By seleniumCheckboxLocator = By.xpath(
-            "//label[text()='Confirm Selenium is checked']/following-sibling::input");
-
-    private By deSelectCheckboxesLocator = By.xpath(
-            "//label[text()='DeSelect only checked']/following-sibling::input");
-
-    private By selectAllCheckboxesLocator = By.xpath(
-            "//label[text()='Select all below checkboxes ']/following-sibling::input");
-    private WebDriver driver;
+    @FindBy(xpath = "//label[text()='Select the languages that you know?']/following-sibling::input")
     private List<WebElement> languageCheckboxes;
+
+    @FindBy(xpath = "//label[text()='Confirm Selenium is checked']/following-sibling::input")
     private WebElement seleniumCheckbox;
+
+    @FindBy(xpath = "//label[text()='DeSelect only checked']/following-sibling::input")
     private List<WebElement> deSelectCheckboxes;
+
+    @FindBy(xpath = "//label[text()='Select all below checkboxes ']/following-sibling::input")
     private List<WebElement> selectAllCheckboxes;
+
+    private WebDriver driver;
+
 
     public List<WebElement> getLanguageCheckboxes() {
         return languageCheckboxes;
@@ -42,32 +42,28 @@ public class TestPage {
 
     public TestPage(WebDriver driver) {
         this.driver = driver;
-
+        PageFactory.initElements(driver, this);
     }
 
     public void start() {
         driver.get(baseUrl);
-        languageCheckboxes = driver.findElements(languageCheckboxesListLocator);
-        seleniumCheckbox = driver.findElement(seleniumCheckboxLocator);
-        deSelectCheckboxes = driver.findElements(deSelectCheckboxesLocator);
-        selectAllCheckboxes = driver.findElements(selectAllCheckboxesLocator);
     }
 
     public void selectJava() {
         languageCheckboxes.get(0).click();
     }
 
-    public void deselectCheckboxes(){
-        for (WebElement element : deSelectCheckboxes){
-            if (element.isSelected()){
+    public void deselectCheckboxes() {
+        for (WebElement element : deSelectCheckboxes) {
+            if (element.isSelected()) {
                 element.click();
             }
         }
     }
 
-    public void selectCheckboxes(){
-        for (WebElement element : selectAllCheckboxes){
-                element.click();
+    public void selectCheckboxes() {
+        for (WebElement element : selectAllCheckboxes) {
+            element.click();
         }
     }
 
