88939e91f1cd781fd85f57cfbce228b5fc8709d1
Ruslan Marchuk <mckruslan@gmail.com>
2022-07-12 16:41:52 +0200

    home work first try

---
+++ src/main/java/dto/PetOrderDto.java
@@ -0,0 +1,17 @@
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PetOrderDto {
+
+    private long petId;
+    private int quantity;
+
+}
--- src/test/java/restassureddemo/RestAssuredExample.java
+++ src/test/java/restassureddemo/RestAssuredExample.java
@@ -2,6 +2,7 @@ package restassureddemo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.PetDto;
+import dto.PetOrderDto;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.path.json.JsonPath;
@@ -65,5 +66,42 @@ public class RestAssuredExample {
 
         Assert.assertEquals(requestPet, responsePet);
 
+    }
+
+    @Test
+    @SneakyThrows
+    public void placeOrder(){
+
+        PetOrderDto requestPetOrder = PetOrderDto.builder().petId(Long.valueOf(petId)).quantity(1).build();
+
+        petOrderId = RestAssured
+                .given()
+                .spec(requestSpecification)
+                .body(new ObjectMapper().writeValueAsString(requestPetOrder))
+                .when()
+                .post("/store/order")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .jsonPath()
+                .getString("id");
+
+        JsonPath jsonResponseOrder = RestAssured
+                .given()
+                .spec(requestSpecification)
+                .when()
+                .get("/store/order/" + petOrderId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        PetOrderDto responsePetOrder = new ObjectMapper().readValue(jsonResponseOrder.prettify(), PetOrderDto.class);
+
+        Assert.assertEquals(requestPetOrder, responsePetOrder);
+
+        System.out.println("petId from request = " + requestPetOrder.getPetId() + "\n"
+                + "petId from response = " + responsePetOrder.getPetId());
     }
 }
